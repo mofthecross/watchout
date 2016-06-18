262cde5c69b19d63743d6d844faeb72f77760e2d
mofthecross <mofthecross@gmail.com>
2016-06-17 17:20:03 -0700

    Implement draggable ship

--- client/watchout.js
+++ client/watchout.js
@@ -5,8 +5,11 @@
 
 var data = [];
 
-var width = 500;
-var height = 500;
+// width/height of board = 800
+// width/height of asteroid = 50
+// width/height of ship = 50
+var width = 800 - 50;
+var height = 800 - 50;
 
 for (var i = 0; i < 10; i++) {
   data.push({'id': i,
@@ -15,21 +18,32 @@ for (var i = 0; i < 10; i++) {
 }
 
 // svg === game board
-var svg = d3.select('body')
+var svg = d3.select('div.board')
             .append('svg')
             .attr('width', width)
             .attr('height', height)
-            .attr('fill', 'red');
+            //.attr('fill', 'black');
+            .style('background-color', 'black');
+
+var ship = svg
+            .append('image')
+            .attr('class', 'draggable')
+            .attr('xlink:href', 'ship.png')
+            .attr('x', width / 2)
+            .attr('y', height / 2)
+            .attr('height', 50)
+            .attr('width', 50);
 
 var update = function(data) {
 
-  var images = svg.selectAll('image')
+  var images = svg.selectAll('image.asteroids')
               .data(data, function(d) {
                 return d.id;
               });
 
   images.enter()
             .append('image')
+            .attr('class', 'asteroids')
             .attr('x', function(d) {
               return d.x;
             })
@@ -60,4 +74,20 @@ setInterval(function() {
     })
   );
 }, 500);
+
+
+//drag
+var mover = function() {
+  d3.select('image.draggable')
+    .attr('x', d3.event.x - parseInt(d3.select('image.draggable').attr('width')) / 2)
+    .attr('y', d3.event.y - parseInt(d3.select('image.draggable').attr('height')) / 2);
+};
+
+var drag = d3.behavior.drag().on('drag', mover);
+
+d3.select('image.draggable')
+  .call(drag);
+
+
+
 
